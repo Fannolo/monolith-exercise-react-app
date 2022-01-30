6ac3c7ce312357e95ea9d69a8aa64b2f220151e2
alessandro <alessandro.fan@welld.ch>
2022-01-30 21:39:05 +0100

    initial configuraton

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const { jsWithBabel } = require("ts-jest/presets");
+
+module.exports = {
+  ...jsWithBabel,
+  cacheDirectory: "./cache",
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text-summary"],
+  globals: {
+    "ts-jest": {
+      babelConfig: "babel.config.js",
+      tsconfig: "tsconfig.test.json",
+      useESM: true,
+    },
+  },
+  moduleFileExtensions: ["js", "json", "ts", "tsx"],
+  preset: "react",
+  setupFilesAfterEnv: ["./tests/unit/setup-files-after-env.js"],
+  testEnvironment: "jsdom",
+  testMatch: ["**/*.test.{js,ts,tsx}"],
+  transform: {
+    ...jsWithBabel.transform,
+    "\\.js$": "./node_modules/react-native/jest/preprocessor.js",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!@react-native|native-base|@react-native-community/picker|native-base-shoutem-theme|react-native-easy-grid|react-native|react-navigation|redux-persist|@codler/react-native-keyboard-aware-scroll-view|@react-navigation/native|@react-native-segmented-control)",
+  ],
+};
